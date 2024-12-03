36fe7de5c832a849e2d7f3cf0680dedefd47c65b
Takano <c0a2309310@edu.teu.ac.jp>
2024-12-03 14:47:59 +0900

    練習4:game Over

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,8 +162,12 @@ def main():
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2 - 150, HEIGHT//2])
                 pg.display.update()
-
+                time.sleep(1)
+                return
             
         if beam is not None and bomb is not None:
             if beam.rct.colliderect(bomb.rct):
@@ -172,8 +176,7 @@ def main():
                 beam = None
                 bird.change_img(6, screen)
                 pg.display.update()
-                time.sleep(1)
-                return
+
 
 
         key_lst = pg.key.get_pressed()
